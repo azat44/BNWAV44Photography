8a91b2c6965fb96c396940448ab1cef526e0cefc
azat44 <azat4beats@gmail.com>
2024-04-15 15:58:08 +0200

    finis 99%v15

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 
 // Fonction pour bloquer l'orientation en mode portrait
 const lockOrientation = () => {
-  if (window.orientation !== 0) { // Vérifie si l'orientation n'est pas en mode portrait
+  if (window.screen.orientation.type === "landscape-primary" || window.screen.orientation.type === "landscape-secondary") { // Vérifie si l'orientation n'est pas en mode portrait
     setTimeout(() => {
-      window.screen.orientation.lock("portrait-primary").catch((error) => {
+      window.screen.orientation.lock("portrait").catch((error) => {
         console.error("Impossible de bloquer l'orientation en mode portrait:", error);
       });
     }, 0);
@@ -20,9 +20,6 @@ lockOrientation();
 
 // Écouter l'événement de changement d'orientation et bloquer l'orientation en mode portrait
 window.addEventListener('orientationchange', lockOrientation);
-
-// Verrouiller la taille de la fenêtre pour empêcher le défilement horizontal
-document.body.style.overflowX = 'hidden';
 
 ReactDOM.render(
   <React.StrictMode>
